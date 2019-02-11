2cd255b2ae8f62c7e715dfbbed8e7e0a0964506e
landongerrits <landon.gerrits@me.com>
2019-02-10 18:11:47 -0800

    first commit

---
+++ services/file_system/project/__init__.py
@@ -0,0 +1,36 @@
+# services/file_system/project/__init__.py
+
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+# instantiate the db
+db = SQLAlchemy()
+
+def create_app(script_info=None):
+
+    # instantiate the app
+    app = Flask(__name__)
+    
+    # enable CORS
+    CORS(app)
+
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    # set up extensions
+    db.init_app(app)
+
+    # register blueprints
+    from project.api.manage_s3 import s3_blueprint
+    app.register_blueprint(s3_blueprint)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+    
+    return app
